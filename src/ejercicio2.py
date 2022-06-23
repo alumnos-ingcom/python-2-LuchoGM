8e46ea6c0264399482a25393eb64742a52b3cb55
LuchoGM <lucholmansilla1@gmail.com>
2022-06-23 03:42:47 -0300

    Agregando inputs fuera de la funcion

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -8,34 +8,36 @@ Implementar una función que obtenga los máximos, mínimos y promedio de
 una secuencia con números, retornando los valores como una tuple.
 Sin utilizar lazos for o las funciones integradas del lenguaje que procesan secuencias.
 """
-def estadistica(secuencia):
+def estadistica(secuencia,lista):
     """
     Esta funcion obtiene los maximos, minimos y promedio de una funcion retornando una tupla
     """
-    contador = 0
-    lista = []
     suma = 0
-    while secuencia > contador:
-        numeros = int(input("Ingrese los numeros: "))
-        lista.append(numeros)
-        lista.sort(reverse = True)
-        contador += 1
-    for i in lista:
-        suma = suma + i
+    indice = 0
+    while indice < len(lista):
+        suma = suma + lista[indice]
+        indice += 1
+    lista.sort(reverse=True)
     promedio = suma/secuencia
     del lista[1:secuencia-1]
     lista.append(promedio)
     tupla = tuple(lista)
     return tupla
 
-
 def principal():
     """
     Esta función es la que se encarga de la parte 'interactiva' del ejercicio
     (La entrada, la llamada al algoritmo y la salida)
     """
-    secuencia = int(input("Ingrese la cantidad de numeros que desea ingresar: "))
-    funcion = estadistica(secuencia)
+  
+    secuencia = int(input("Ingrese la cantidad de numeros que desea ingresar: "))     
+    contador = 0
+    lista = []
+    while secuencia > contador:
+        numeros = int(input("Ingrese los numeros: "))
+        lista.append(numeros)
+        contador += 1
+    funcion = estadistica(secuencia,lista)
     print (funcion)
 
 if __name__ == "__main__":
