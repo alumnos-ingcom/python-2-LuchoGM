25990b916e8470638ca2e8021172b3ae99aff25d
LuchoGM <lucholmansilla1@gmail.com>
2022-06-23 02:23:55 -0300

    Agegando plantilla ejercicio2

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -1,4 +1,17 @@
-def estadistica(secuencia):   
+################
+# Luciano Mansilla - @LuchoGM
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+"""
+Implementar una función que obtenga los máximos, mínimos y promedio de
+una secuencia con números, retornando los valores como una tuple.
+Sin utilizar lazos for o las funciones integradas del lenguaje que procesan secuencias.
+"""
+def estadistica(secuencia):
+    """
+    Esta funcion obtiene los maximos, minimos y promedio de una funcion retornando una tupla
+    """
     contador = 0
     lista = []
     suma = 0
@@ -14,6 +27,16 @@ def estadistica(secuencia):
     lista.append(promedio)
     tupla = tuple(lista)
     return tupla
-secuencia = int(input("Ingrese la cantidad de numeros que desea ingresar: "))
-funcion = estadistica(secuencia)
-print (funcion)
+
+
+def principal():
+    """
+    Esta función es la que se encarga de la parte 'interactiva' del ejercicio
+    (La entrada, la llamada al algoritmo y la salida)
+    """
+    secuencia = int(input("Ingrese la cantidad de numeros que desea ingresar: "))
+    funcion = estadistica(secuencia)
+    print (funcion)
+
+if __name__ == "__main__":
+    principal()
